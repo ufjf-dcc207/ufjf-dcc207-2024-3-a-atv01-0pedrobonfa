48d90f560a5c786a78d377ce2ce1d00c124ba551
Pedro Bonfa <pedro.bonfa@estudante.ufjf.br>
2024-11-11 20:53:25 -0300

    cria componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import Disciplina from "./Disciplina";
+
 function App() {
-  return <h1>Olá mundo!</h1>;
+  return (
+    <div>
+      <h1>Olá mundo!</h1>
+      <Disciplina/>
+    </div>
+
+  )
 }
 
 export default App;
