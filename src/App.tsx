d30e470eca007c4985e208debd27d0160747ec06
Pedro Bonfa <pedro.bonfa@estudante.ufjf.br>
2024-11-11 20:53:25 -0300

    adiciona props

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ function App() {
   return (
     <div>
       <h1>Olá mundo!</h1>
-      <Disciplina/>
+      <Disciplina codigo="DCC07" nome="Desenvolvimento Web I"/>
+      <Disciplina codigo="DCC06" nome="Desenvolvimento Web II"/>
+      <Disciplina codigo="DCC07" nome="Desenvolvimento Web Front-End"/>
     </div>
 
   )
--- src/Disciplina.tsx
+++ src/Disciplina.tsx
@@ -1,11 +1,16 @@
 import './Disciplina.css'
-function Disciplina(){
-    const codigo = "DCC207";
-    const nome = "Desenvolvimento Web Front-End";
+
+type DisciplinaProps = {
+    codigo: string;
+    nome: string;
+}
+
+function Disciplina(props:DisciplinaProps){
+
     return <div className="disciplina">
         
-        <div>{codigo}</div>
-        <div>{nome} Web Front-End</div>
+        <div>{props.codigo}</div>
+        <div>{props.nome}</div>
 
     </div>
 }
